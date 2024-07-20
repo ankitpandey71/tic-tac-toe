2cb00187138289f27fd5255b0199329ba1a1988f
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-07-21 00:23:08 +0530

    [ADD] boxes

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -7,7 +7,23 @@ const TicTacToe = () => {
   return (
     <div className="container">
       <h1 className="title">Tic Tac Toe in React</h1>
-      <div className="board"></div>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
       <button className="reset"> Reset</button>
     </div>
   );
