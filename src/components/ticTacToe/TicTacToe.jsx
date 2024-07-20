0ac61e9a5d7617263ce730c870d3ad304f9bcd74
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-07-21 00:19:28 +0530

    [ADD] css styling

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./TicTacToe.css";
+import circle_icon from "../assets/circle.png";
+import cross_icon from "../assets/cross.png";
 
 const TicTacToe = () => {
-  return <div>TicTacToe</div>;
+  return (
+    <div className="container">
+      <h1 className="title">Tic Tac Toe in React</h1>
+      <div className="board"></div>
+      <button className="reset"> Reset</button>
+    </div>
+  );
 };
 
 export default TicTacToe;
